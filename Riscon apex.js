e9317b0ff5829e3fbc73016e30ff8d783f9eec23
Kamdar-Wolf <kamdar@seznam.cz>
2025-11-26 21:14:29 +0100

    edit

--- Riscon apex.js
+++ Riscon apex.js
@@ -6,8 +6,8 @@
 // @match        https://*/ords/*/f?p=110:*
 // @match        https://www.riscon.cz/go/f?p=110*
 // @icon         https://www.riscon.cz//i/favicon.ico
-// @updateURL    https://raw.githubusercontent.com/Kamdar-Wolf/Skripty/master/Riscon%3A%20JSON.js
-// @downloadURL  https://raw.githubusercontent.com/Kamdar-Wolf/Skripty/master/Riscon%3A%20JSON.js
+// @updateURL    https://raw.githubusercontent.com/Kamdar-Wolf/Skripty/master/Riscon%20apex.js
+// @downloadURL  https://raw.githubusercontent.com/Kamdar-Wolf/Skripty/master/Riscon%20apex.js
 // @grant        none
 // ==/UserScript==
 
