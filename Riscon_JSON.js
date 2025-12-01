a58531fbfa724546c0583c787d3feb80f0394bf2
Kamdar-Wolf <kamdar@seznam.cz>
2025-12-01 22:40:36 +0100

    Edit

--- Riscon_JSON.js
+++ Riscon_JSON.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Riscon: JSON
-// @namespace    Automatické doplňování ve formátu JSON
-// @version      6.4
+// @namespace    Přestup URL
+// @version      6.7
 // @description  Vyplní formulář z JSONu a vytěží data v APEX stylu.
 // @match        https://*/ords/*/f?p=110:*
 // @match        https://www.riscon.cz/go/f?p=110*
 // @icon         https://www.riscon.cz//i/favicon.ico
-// @updateURL    https://raw.githubusercontent.com/Kamdar-Wolf/Skripty/refs/heads/main/Riscon_JSON.js
-// @downloadURL  https://raw.githubusercontent.com/Kamdar-Wolf/Skripty/refs/heads/main/Riscon_JSON.js
+// @updateURL    https://raw.githubusercontent.com/Kamdar-Wolf/Prace/master/Riscon/JSON.user.js
+// @downloadURL  https://raw.githubusercontent.com/Kamdar-Wolf/Prace/master/Riscon/JSON.user.js
 // @grant        none
 // ==/UserScript==
 (function () {
